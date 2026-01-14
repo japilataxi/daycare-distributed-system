2ead194e99405e23834cc42ad991cf90b61bb8ff
japilataxi <japilataxi@uce.edu.ec>
2026-01-14 09:28:13 -0500

    chore: remove legacy architecture and bootstrap turborepo with NestJS auth service

---
+++ apps/auth-service/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Auth Service')
+    .setDescription('Authentication and authorization service')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
---
+++ infra/terraform/envs/prod/terraform.tfvars
@@ -0,0 +1,6 @@
+environment        = "prod"
+cpu                = 512
+memory             = 1024
+desired_count      = 2
+enable_multi_az    = true
+db_instance_class  = "db.t3.small"
